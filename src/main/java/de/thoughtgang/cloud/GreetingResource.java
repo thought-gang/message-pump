f290053489570e90d70d940336cd38cfbeb9aa82
Marie Rümelin <marie.ruemelin@otto.de>
2022-01-26 17:48:39 +0100

    added post request

--- src/main/java/de/thoughtgang/cloud/GreetingResource.java
+++ src/main/java/de/thoughtgang/cloud/GreetingResource.java
@@ -1,6 +1,8 @@
 package de.thoughtgang.cloud;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -8,7 +10,8 @@ import javax.ws.rs.core.MediaType;
 @Path("/hello")
 public class GreetingResource {
 
-    @GET
+    @POST
+    @Consumes(MediaType.TEXT_PLAIN)
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "Hello RESTEasy";
---
+++ src/main/java/de/thoughtgang/cloud/WebServiceInletResource.java
@@ -0,0 +1,39 @@
+package de.thoughtgang.cloud;
+
+import org.jboss.logging.Logger;
+
+import io.vertx.core.http.HttpServerRequest;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Request;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.stream.Collectors;
+
+
+@Path("/inlet")
+public class WebServiceInletResource {
+
+    private static final Logger LOG = Logger.getLogger(WebServiceInletResource.class);
+
+    @POST
+    @Consumes(MediaType.WILDCARD)
+    @Produces(MediaType.TEXT_PLAIN)
+    public String submit(@Context HttpServerRequest request, InputStream inputStream) {
+
+        Object test = request.headers();
+        String contentType = request.getHeader("Content-Type");
+        InputStreamReader reader = new InputStreamReader(inputStream);
+        String body = new BufferedReader(reader).lines().collect(Collectors.joining("\n"));
+        LOG.info(contentType + " ----" + body);
+
+        return "hello";
+    }
+
+ }
